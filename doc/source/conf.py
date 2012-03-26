8c5b80c9b0fc537214673b689508f643da624735
David Townshend <aquavitae69@gmail.com>
2012-03-26 09:56:46 +0200

    [mq]: qt docs updated

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -33,6 +33,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.todo',
               'sphinx.ext.autodoc',
+              'sphinx.ext.autosummary',
               'sphinx.ext.doctest',
               'sphinx.ext.intersphinx']
 
