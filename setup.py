e20800af3920303628e8a894e10ad2b92c9c3f87
David Townshend <aquavitae69@gmail.com>
2013-02-12 10:47:20 +0200

    Update trove classifiers

--- setup.py
+++ setup.py
@@ -36,7 +36,10 @@ classifiers = [
     'Programming Language :: Python :: 3.0',
     'Programming Language :: Python :: 3.1',
     'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Topic :: Database',
     'Topic :: Database :: Database Engines/Servers',
+    'Topic :: Software Development',
     'Topic :: Software Development :: Libraries :: Python Modules'
 ]
 
